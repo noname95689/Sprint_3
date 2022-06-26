91514d064fe5b253dec09055ceacf62d8ac45ffe
Evgenii Buchnev <noname95689@yandex.ru>
2022-06-26 14:00:01 +0300

    Проект 3 спринта

---
+++ src/main/java/model/CourierId.java
@@ -0,0 +1,15 @@
+public class CourierId {
+
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public CourierId() {
+    }
+}
